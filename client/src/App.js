27eb94d30e3f0564fa1068f804481ed94c2b8edf
cyebukayire <peacecyebukayire@gmail.com>
2022-04-28 15:45:24 +0200

    chore: connect server to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const [data, setData] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <p>{!data ? "Loading..." : data}</p> */}
+        
+      <form action="../../post" method="post" 
+              className="form">
+          {/* <button type="submit">Connected?</button> */}
+        </form>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({message: 'Welcome to the server'})
+})
+
+app.get("/api", (req, res) => {
+    res.json({message: 'Hello From Server'})
+})
+
+app.post("/post", (req, res) => {
+    console.log("Connected to React");
+    res.redirect("/");
+  });
+  
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
 })
